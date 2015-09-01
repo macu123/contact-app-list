5a9a5658164bb2c2c0191e7c558afd5806faaf6e
John <chenjiapeng12345@gmail.com>
2015-09-01 17:37:24 +0000

    finish all tasks except the task 7

--- contact.rb
+++ contact.rb
@@ -8,7 +8,7 @@ class Contact
   end
  
   def to_s
-    "#{@name} (#{@email})"
+    "#{name} (#{email})"
   end
  
   ## Class Methods
@@ -17,13 +17,33 @@ class Contact
       contact = Contact.new(name, email)
       Contact_database.add_contact(contact)
     end
+
+    def duplicate?(email)
+      contacts_arr = Contact_database.read_all_contacts
+      contacts_arr.each do |contact_ele|
+        return true if contact_ele[2] == email
+      end
+      false
+    end
  
-    def find(term)
-      # TODO: Will find and return contacts that contain the term in the first name, last name or email
+    def find(search_term)
+      contacts_arr = Contact_database.read_all_contacts
+      contacts_arr.each do |contact_ele|
+        if (contact_ele[1].include? search_term) || (contact_ele[2].include? search_term)
+          return contact_ele
+        end
+      end
+      false
     end
  
     def all
-      Contact_database.read_all_contacts
+      contacts_arr = Contact_database.read_all_contacts
+      contacts_str_arr = []
+      contacts_arr.each do |contact_ele|
+        contact = Contact.new(contact_ele[1], contact_ele[2])
+        contacts_str_arr << contact_ele[0] + ": " + contact.to_s
+      end
+      contacts_str_arr
     end
     
     def show(id)
--- contact_list.rb
+++ contact_list.rb
@@ -9,6 +9,34 @@ class Contact_list
     STDIN.gets.chomp
   end
 
+  # get users phone numbers
+  # def self.get_phone_num
+  #   phone_num = {}
+  #   while true
+  #     puts "Do you have a phone number? (Y/N)"
+  #     command = get_user_input
+  #     case command
+  #     when "Y"
+  #       puts "Please input the label of the phone number now:"
+  #       label = get_user_input
+  #       puts "Please input the number itself now:"
+  #       number = get_user_input
+  #       phone_num[label] = number
+  #     when "N"
+  #       break
+  #     else
+  #       puts "Sorry I don't understand your command!"
+  #     end
+  #   end
+  #   phone_num
+  # end
+
+  # print contact details in separate lines
+  def self.puts_details(contact_ele)
+    puts "Name: #{contact_ele[1]}"
+    puts "Email: #{contact_ele[2]}"
+  end
+
   # determine which command to execute
   def self.which_command_to_execute(command)
     case command
@@ -29,38 +57,48 @@ class Contact_list
     puts "find - Find a contact"
   end
 
+  # create a contact
   def self.create_contact
-    puts "Please input the full name now:"
-    name = get_user_input
     puts "Please input the email now:"
     email = get_user_input
+    if Contact.duplicate?(email)
+      puts "The contact already exists and cannot be created"
+      return
+    else
+      puts "Please input the full name now:"
+      name = get_user_input
+    end
 
-    #create a contact
     puts Contact.create(name, email)
   end
-
+  
+  # list all contacts
   def self.list_contacts
-    # list all contacts
-    contacts = Contact.all
-    contacts.each do |contact|
-      puts contact.inspect
+    contacts_str_arr = Contact.all
+    contacts_str_arr.each do |contact_str|
+      puts contact_str
     end
   end
 
   # show a contact with ID
   def self.show_contact  
-    contact = Contact.show(ARGV[1])
-    if contact
-      puts "Name: #{contact[1]}"
-      puts "email: #{contact[2]}"
+    contact_ele = Contact.show(ARGV[1])
+    if contact_ele
+      puts_details(contact_ele)
     else
       puts "Sorry we couldn't find a contact with ID #{ARGV[1]}"
     end
   end
 
+  # find a contact with term
   def self.find_contact
-    # find a contact with term
-    Contact.find(ARGV[1])
+    search_term = ARGV[1]
+    contact_ele = Contact.find(search_term)
+    if contact_ele
+      puts_details(contact_ele)
+    else
+      puts "Sorry we couldn't find a contact with the search term #{search_term}"
+    end
   end
 
 end
